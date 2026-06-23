8cf026ffc7f11006f52ddfe263e9b3b508ea1653
duhamel <elie.duhamel@grenoble-inp.org>
2026-06-23 21:55:49 +0200

    addtest

---
+++ tests/test_csg.rs
@@ -0,0 +1,16 @@
+use VCSG::csg;
+use VCSG::csg::core_volu::VolumeCSG;
+use VCSG::csg::point::Vector;
+use VCSG::csg::random_generator::RandomGeneratorPointBox;
+use VCSG::csg::volume::{Volume, VolumePrimaire};
+
+#[test]
+fn test_csg() {
+
+
+    let boxe=csg::volume::Box::new(Vector::new([-1.,-1.,-1.]),Vector::new([2.,2.,2.]));
+    let sphe=csg::volume::Sphere::new(Vector::new([-1.,0.,0.]), 1.);
+    let volume=csg::operator::csg::Moins::new(Box::new(VolumeCSG::VolumePrimaire(VolumePrimaire::Box(boxe))),Box::new(VolumeCSG::VolumePrimaire(VolumePrimaire::Sphere(sphe))));
+   let volu= volume.get_estimated_volume_single(1000, &mut RandomGeneratorPointBox::new(csg::volume::Box::new(Vector::new([-1.,-1.,-1.]),Vector::new([2.,2.,2.]))));
+    println!("volu: {:?}", volu);
+}
